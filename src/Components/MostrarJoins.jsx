c41584f32849e2b1d8d4fff0ccbb2ec134673f39
Joaquin <FacundoJoaGL@gmail.com>
2024-07-22 13:40:07 -0300

    cambios en el build

--- src/Components/MostrarJoins.jsx
+++ src/Components/MostrarJoins.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Footer, Header } from '../Constants'
-import { Button, Table } from 'react-bootstrap'
+import { Table } from 'react-bootstrap'
 import axios from 'axios'
-import { Link } from 'react-router-dom'
+
 
 export const MostrarJoins = () => {
     const [array, setArray] = useState([])
